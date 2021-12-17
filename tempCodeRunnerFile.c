6ab9f77d56b78fb54e589d0e78d5d00d6907eb2e
Shivraj Anand <shivrajanand022002@gmail.com>
2021-12-17 23:13:59 +0530

    deleted useless file tempcoderunner

--- tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-while (new_string[(strlen(new_string)-1)] == ' ')
-    // {
-    //     for (int k = 0; k < strlen(new_string); k++)
-    //     {
-    //         new_string[k] = new_string[k+1];
-
-    //     }
-        
-    // }
